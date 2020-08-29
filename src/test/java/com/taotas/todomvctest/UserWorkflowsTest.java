2d30b905000481a2ab790dbb4039c429d9d40777
AlexandraUstiugova <alexandrauuuu@gmail.com>
2020-08-29 15:10:54 +0300

    fixed according recommendations

--- src/test/java/com/taotas/todomvctest/UserWorkflowsTest.java
+++ src/test/java/com/taotas/todomvctest/UserWorkflowsTest.java
@@ -25,7 +25,7 @@ public class UserWorkflowsTest {
         itemsLeftShouldBe(3);
 
         // Edit
-        changeTodo("b", " edited").pressEnter();
+        startEdit("b", " edited").pressEnter();
 
         // Complete and Clear
         todo("b edited").find(".toggle").click();
@@ -33,7 +33,7 @@ public class UserWorkflowsTest {
         todosShouldBe("a", "c");
 
         // Cancel edit
-        changeTodo("c", " to be canceled").pressEscape();
+        startEdit("c", " to be canceled").pressEscape();
 
         // Delete
         todo("c").hover().find(".destroy").click();
@@ -43,14 +43,14 @@ public class UserWorkflowsTest {
 
     private ElementsCollection todos = $$("#todo-list>li");
 
-    private void add(String... todoTexts) {
-        for (String text: todoTexts) {
+    private void add(String... texts) {
+        for (String text: texts) {
             element("#new-todo").append(text).pressEnter();
         }
     }
 
-    private void todosShouldBe(String... todoTexts) {
-        todos.shouldHave(exactTexts(todoTexts));
+    private void todosShouldBe(String... texts) {
+        todos.shouldHave(exactTexts(texts));
     }
 
     private void itemsLeftShouldBe(int number) {
@@ -58,7 +58,7 @@ public class UserWorkflowsTest {
                 Integer.toString(number)));
     }
 
-    private SelenideElement todoBy (Condition condition) {
+    private SelenideElement todoBy(Condition condition) {
         return todos.findBy(condition);
     }
 
@@ -66,8 +66,8 @@ public class UserWorkflowsTest {
         return todoBy(exactText(text));
     }
 
-    private SelenideElement changeTodo(String oldText, String newText) {
-        todo(oldText).doubleClick();
-        return todoBy(cssClass("editing")).find(".edit").append(newText);
+    private SelenideElement startEdit(String text, String textToAdd) {
+        todo(text).doubleClick();
+        return todoBy(cssClass("editing")).find(".edit").append(textToAdd);
     }
 }
